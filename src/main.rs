1241dbe24547f882e9952a7bf158b303e3027673
Haf <haf@protonmail.ch>
2025-07-20 22:14:06 +0200

    refactor: use a dispatcher instead of repl (closes #2)

--- src/main.rs
+++ src/main.rs
@@ -15,33 +15,50 @@ async fn main() {
         openai::Credentials::new(&env.openrouter_token, "https://openrouter.ai/api/v1");
     let bot = Bot::new(env.teloxide_token);
 
-    teloxide::repl(bot, move |bot: Bot, msg: Message| {
-        // TODO: can't use an async closure?
-        let credentials = credentials.clone();
-        async move {
+    let schema = Update::filter_message().endpoint(answer);
 
-        let mut messages = vec![openai::chat::ChatCompletionMessage {
+    Dispatcher::builder(bot, schema)
+        .dependencies(dptree::deps![credentials])
+        .build()
+        .dispatch()
+        .await;
+}
+
+async fn answer(
+    bot: Bot,
+    msg: Message,
+    credentials: openai::Credentials,
+) -> Result<(), anyhow::Error> {
+    let mut messages = vec![openai::chat::ChatCompletionMessage {
             role: openai::chat::ChatCompletionMessageRole::System,
             content: Some("You're a helpful telegram bot. You don't reply with huge walls of text, but try to be concise and to the point".into()),
             ..Default::default()
         }];
 
-        if let Some(text) = msg.text() {
-            messages.push(openai::chat::ChatCompletionMessage {
-                role: openai::chat::ChatCompletionMessageRole::User,
-                content: Some(text.into()),
-                ..Default::default()
-            });
+    if let Some(text) = msg.text() {
+        messages.push(openai::chat::ChatCompletionMessage {
+            role: openai::chat::ChatCompletionMessageRole::User,
+            content: Some(text.into()),
+            ..Default::default()
+        });
 
-            let chat_completion = openai::chat::ChatCompletion::builder("google/gemini-2.5-pro", messages).credentials(credentials.clone()).create().await.expect("TODO this will crash the whole program right?");
+        let chat_completion =
+            openai::chat::ChatCompletion::builder("google/gemini-2.5-pro", messages)
+                .credentials(credentials.clone())
+                .create()
+                .await?;
 
-            eprintln!("{chat_completion:#?}");
-            let returned_message = chat_completion.choices.first().unwrap().message.clone();
-            bot.send_message(msg.chat.id, returned_message.content.unwrap()).await?;
-        }
-        Ok(())
-    }})
-    .await;
+        let returned_message = chat_completion
+            .choices
+            .first()
+            .unwrap()
+            .message
+            .clone()
+            .content
+            .unwrap();
+        bot.send_message(msg.chat.id, returned_message).await?;
+    }
+    Ok(())
 }
 
 #[derive(serde::Deserialize)]
