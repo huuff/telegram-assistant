c7042cf323da391407eb4bc14009c9e6fe51e591
Haf <haf@protonmail.ch>
2025-07-20 22:22:13 +0200

    fix: correct clippy check

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
             treefmt = treefmt-build.wrapper;
           };
         };
-        inherit (nix-checks.lib.${system}) checks;
+        inherit (nix-checks.lib.${system}) checks rustChecks;
       in
       {
         checks = {
@@ -62,7 +62,7 @@
           statix = checks.statix ./.;
           deadnix = checks.deadnix ./.;
           flake-checker = checks.flake-checker ./.;
-          clippy = checks.clippy ./.;
+          clippy = rustChecks.clippy { toolchain = rustPkgs; } ./.;
         };
 
         # for nix fmt
