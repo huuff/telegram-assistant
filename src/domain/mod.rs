8dfbc3355962d8611fdefa2fd6431020028b2a21
Haf <haf@protonmail.ch>
2025-07-28 20:36:30 +0200

    feat: gave the assistant some knowledge of the user

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,2 +1,3 @@
 pub mod chat;
 pub mod prompts;
+pub mod user;
--- src/domain/prompts.rs
+++ src/domain/prompts.rs
@@ -1,17 +1,36 @@
 use askama::Template;
+use bon::Builder;
 
-#[derive(Template)]
+use super::user::User;
+
+#[derive(Template, Builder)]
 #[template(path = "system_prompt.md")]
 pub struct SystemPrompt {
+    #[builder(default = jiff::Zoned::now().in_tz("UTC").expect("should always be possible to construct a time in UTC"))]
     time: jiff::Zoned,
+    user: User,
 }
 
-impl Default for SystemPrompt {
-    fn default() -> Self {
-        Self {
-            time: jiff::Zoned::now()
-                .in_tz("UTC")
-                .expect("should always be able to construct a timezone in UTC"),
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn prompt_snapshot() {
+        // ARRANGE
+        let prompt = SystemPrompt::builder()
+            .time("2025-07-28T00:00:00[UTC]".parse().unwrap())
+            .user(User {
+                id: "1".into(),
+                name: "User".into(),
+                preferred_language: Some("Klingon".into()),
+            })
+            .build();
+
+        // ACT
+        let text = prompt.render().unwrap();
+
+        // ASSERT
+        insta::assert_snapshot!(text);
     }
 }
